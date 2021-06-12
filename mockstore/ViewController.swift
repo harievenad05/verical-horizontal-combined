1419cf384da5fc54b58be75d040915b0fe51499e
Haribalan <haribalan.r@photoninfotech.net>
2021-06-12 13:35:57 +0530

    Initial Commit

---
+++ mockstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mockstore
+//
+//  Created by Harie Venad on 12/06/21.
+//  Copyright © 2021 Harie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
